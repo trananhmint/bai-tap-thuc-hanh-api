2bcea7b3ce939405f23d0f13fc56fb3d3beed534
trananhmint <trananhmint@gmail.com>
2026-01-14 16:25:19 +0700

    chore: initialize ASP.NET Core Web API project

---
+++ BaiTapThucHanhAPI/Controllers/DonViTinhController.cs
@@ -0,0 +1,103 @@
+﻿using BaiTapThucHanhAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapThucHanhAPI.Controllers;
+
+[ApiController]
+[Route("api/don-vi-tinh")]
+public class DonViTinhController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public DonViTinhController(AppDbContext context)
+    {
+        _context = context;
+    }
+    
+    [HttpGet]
+    public IActionResult GetAllDonViTinh()
+    {
+        List<DonViTinh> donViTinhList = _context.DonViTinh.ToList();
+        return Ok(donViTinhList);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetDonViTinhById(int id)
+    {
+        if (id <= 0)
+        {
+            return BadRequest("Invalid Id!");
+        }
+        var entity = _context.DonViTinh.Find(id);
+        if (entity == null)
+        {
+            return NotFound("DonViTinh not found!");
+        }
+        return Ok(entity);
+    }
+    
+    [HttpPost]
+    public IActionResult AddDonViTinh([FromBody] DonViTinhDTO donViTinh)
+    {
+        if (donViTinh ==  null)
+        {
+            return BadRequest("DonViTinh is null!");
+        } else if (string.IsNullOrWhiteSpace(donViTinh.TenDonViTinh))
+        {
+            return BadRequest("TenDonViTinh is null or empty!");
+        }
+        bool isExist = _context.DonViTinh.Any(x => x.TenDonViTinh == donViTinh.TenDonViTinh);
+        if (isExist)
+        {
+            return BadRequest("DonViTinh is already existed!");
+        }
+        _context.DonViTinh.Add(new DonViTinh()
+        {
+            TenDonViTinh = donViTinh.TenDonViTinh,
+            GhiChu = donViTinh.GhiChu,
+        });
+        _context.SaveChanges();
+        return Ok("Add Don Vi Tinh Successfully");
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateDonViTinh(int id, [FromBody] DonViTinhDTO donViTinh)
+    {
+        if (id <= 0)
+        {
+            return BadRequest("Invalid Id!");
+        }
+        var entity = _context.DonViTinh.Find(id);
+        if (entity == null)
+        {
+            return NotFound("DonViTinh not found!");
+        }
+        bool isTenDonViTinhExist = _context.DonViTinh.Any(x => x.TenDonViTinh == donViTinh.TenDonViTinh);
+        if (isTenDonViTinhExist)
+        {
+            return BadRequest("TenDonViTinh is existed!");
+        }
+        entity.TenDonViTinh = donViTinh.TenDonViTinh;
+        entity.GhiChu = donViTinh.GhiChu;
+        _context.SaveChanges();
+        return Ok("Update Don Vi Tinh Successfully");
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteDonViTinh(int id)
+    {
+        if (id <= 0)
+        {
+            return BadRequest("Invalid Id!");
+        }
+        var entity = _context.DonViTinh.Find(id);
+        if (entity == null)
+        {
+            return NotFound("DonViTinh not found!");
+        }
+
+        _context.DonViTinh.Remove(entity);
+        _context.SaveChanges();
+        return Ok("Delete Don Vi Tinh Successfully");
+    }
+}
---
+++ BaiTapThucHanhAPI/Controllers/LoaiSanPhamController.cs
@@ -0,0 +1,115 @@
+﻿using BaiTapThucHanhAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapThucHanhAPI.Controllers;
+
+[ApiController]
+[Route("api/loai-san-pham")]
+public class LoaiSanPhamController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public LoaiSanPhamController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult GetAllLoaiSanPham()
+    {
+        return Ok(_context.LoaiSanPham.ToList());
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetLoaiSanPhamById(int id)
+    {
+        if (id <= 0)
+        {
+            return BadRequest("Invalid Id!");
+        }
+
+        var entity = _context.LoaiSanPham.Find(id);
+        if (entity == null)
+        {
+            return NotFound("Loai san pham not found!");
+        }
+
+        return Ok(entity);
+    }
+
+    [HttpPost]
+    public IActionResult AddLoaiSanPham([FromBody] LoaiSanPhamDTO loaiSanPham)
+    {
+        if (loaiSanPham == null)
+        {
+            return BadRequest("Loai san pham null!");
+        }
+        else if (string.IsNullOrWhiteSpace(loaiSanPham.TenLSP) || string.IsNullOrWhiteSpace(loaiSanPham.MaLSP))
+        {
+            return BadRequest("Loai san pham information null or empty!");
+        }
+
+        bool isExist = _context.LoaiSanPham.Any(x => x.MaLSP == loaiSanPham.MaLSP || x.TenLSP == loaiSanPham.TenLSP);
+        if (isExist)
+        {
+            return BadRequest("Loai san pham is already existed!");
+        }
+
+        _context.LoaiSanPham.Add(new LoaiSanPham()
+        {
+            TenLSP = loaiSanPham.TenLSP,
+            MaLSP = loaiSanPham.MaLSP,
+            GhiChu = loaiSanPham.GhiChu
+        });
+        _context.SaveChanges();
+        return Ok("Loai san pham Added Successfully!");
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateLoaiSanPham(int id, [FromBody] LoaiSanPhamDTO loaiSanPham)
+    {
+        if (loaiSanPham == null)
+        {
+            return BadRequest("Loai san pham null!");
+        }
+        else if (string.IsNullOrWhiteSpace(loaiSanPham.TenLSP) || string.IsNullOrWhiteSpace(loaiSanPham.MaLSP))
+        {
+            return BadRequest("Loai san pham information null or empty!");
+        }
+        var entity =  _context.LoaiSanPham.Find(id);
+        if (entity == null)
+        {
+            return NotFound("Loai san pham not found!");
+        }
+        bool isExistedInfo = _context.LoaiSanPham.Any(x => x.MaLSP == loaiSanPham.MaLSP || x.TenLSP == loaiSanPham.TenLSP);
+        if (isExistedInfo)
+        {
+            return BadRequest("Loai san pham is already existed!");
+        }
+        entity.MaLSP = loaiSanPham.MaLSP;
+        entity.TenLSP = loaiSanPham.TenLSP;
+        entity.GhiChu = loaiSanPham.GhiChu;
+        _context.LoaiSanPham.Update(entity);
+        _context.SaveChanges();
+        return Ok("Loai san pham Updated Successfully!");
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteLoaiSanPham(int id)
+    {
+        if (id <= 0)
+        {
+            return BadRequest("Invalid Id!");
+        }
+
+        var entity = _context.LoaiSanPham.Find(id);
+        if (entity == null)
+        {
+            return NotFound("Loai san pham not found!");
+        }
+
+        _context.LoaiSanPham.Remove(entity);
+        _context.SaveChanges();
+        return Ok("Delete Don Vi Tinh Successfully");
+    }
+}
