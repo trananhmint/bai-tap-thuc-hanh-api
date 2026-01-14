4e220dc82273eb15f15060e86ac3bc97623ef247
trananhmint <trananhmint@gmail.com>
2026-01-14 17:24:06 +0700

    feat(master-data): add CRUD APIs for DonViTinh

---
+++ BaiTapThucHanhAPI/Models/DonViTinh.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BaiTapThucHanhAPI.Controllers;
+
+[Table ("tbl_DM_Don_Vi_Tinh")]
+public class DonViTinh
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(70)]
+    [Column("Ten_Don_Vi_Tinh")]
+    public string TenDonViTinh { get; set; } = null!;
+    
+    [StringLength(1000)]
+    [Column("Ghi_Chu")]
+    public string? GhiChu { get; set; }
+}
