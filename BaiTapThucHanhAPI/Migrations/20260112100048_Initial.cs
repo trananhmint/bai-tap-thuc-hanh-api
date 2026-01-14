f1a5447ba04e710a07527d9c31284de413e25850
trananhmint <trananhmint@gmail.com>
2026-01-14 16:31:32 +0700

    chore(db): setup EF Core DbContext and initial migration

---
+++ BaiTapThucHanhAPI/Migrations/20260112100048_Initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BaiTapThucHanhAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tbl_DM_Don_Vi_Tinh",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Ten_Don_Vi_Tinh = table.Column<string>(type: "nvarchar(70)", maxLength: 70, nullable: false),
+                    Ghi_Chu = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tbl_DM_Don_Vi_Tinh", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tbl_DM_Don_Vi_Tinh");
+        }
+    }
+}
---
+++ BaiTapThucHanhAPI/Models/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using BaiTapThucHanhAPI.Controllers;
+using Microsoft.EntityFrameworkCore;
+
+namespace BaiTapThucHanhAPI.Models;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+        
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+    
+    public virtual DbSet<DonViTinh> DonViTinh { get; set; }
+    public virtual DbSet<LoaiSanPham> LoaiSanPham { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(connectionString: "Data Source=(local);database=BaiTapThucHanh;uid=sa;pwd=12345;TrustServerCertificate=True;MultipleActiveResultSets=True");
+    }
+}   
