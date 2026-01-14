8b28f97dc6b3d55a6fa071b3e3b4a03ec8223b8d
trananhmint <trananhmint@gmail.com>
2026-01-14 17:30:31 +0700

    feat(master-data): add CRUD APIs for LoaiSanPham

---
+++ BaiTapThucHanhAPI/Models/LoaiSanPham.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BaiTapThucHanhAPI.Models;
+
+[Table("tbl_DM_Loai_San_Pham")]
+public class LoaiSanPham
+{
+    [Key]
+    [Required]
+    [Column("Ma_LSP")]
+    public string MaLSP { get; set; } = null!;
+    
+    [Required]
+    [Column("Ten_LSP")]
+    public string TenLSP { get; set; } = null!;
+    
+    [Column("Ghi_Chu")]
+    public string? GhiChu { get; set; }
+}
---
+++ BaiTapThucHanhAPI/Models/LoaiSanPhamDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BaiTapThucHanhAPI.Models;
+
+public class LoaiSanPhamDTO
+{
+    public string MaLSP { get; set; }
+    public string TenLSP { get; set; }
+    public string? GhiChu { get; set; }
+    
+}
